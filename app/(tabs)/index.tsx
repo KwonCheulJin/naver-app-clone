06ec8255a9d9aa823ee45a0de3c34d2bdc9fde9b
KwonCheulJin <chkftm12@gmail.com>
2025-02-18 16:59:14 +0900

    브라우저 기능 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { router } from 'expo-router';
 import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
+import { ShouldStartLoadRequest } from 'react-native-webview/lib/WebViewTypes';
 
 const styles = StyleSheet.create({
   safearea: {
@@ -9,27 +10,28 @@ const styles = StyleSheet.create({
   },
 });
 export default function HomeScreen() {
+  const handleOnShouldStartLoadWithRequest = (req: ShouldStartLoadRequest) => {
+    console.log('home - request', req);
+    if (
+      req.url.startsWith('https://m.naver.com') ||
+      req.mainDocumentURL?.startsWith('https://m.naver.com')
+    ) {
+      return true;
+    }
+
+    if (req.url !== null && req.url.startsWith('https://')) {
+      router.navigate({ pathname: 'browser', params: { initialUrl: req.url } });
+      return false;
+    }
+    return true;
+  };
   return (
     <SafeAreaView style={styles.safearea}>
       <WebView
         source={{ uri: 'https://m.naver.com' }}
         showsVerticalScrollIndicator={false}
         showsHorizontalScrollIndicator={false}
-        onShouldStartLoadWithRequest={req => {
-          console.log('home - request', req);
-          if (
-            req.url.startsWith('https://m.naver.com') ||
-            req.mainDocumentURL?.startsWith('https://m.naver.com')
-          ) {
-            return true;
-          }
-
-          if (req.url !== null && req.url.startsWith('https://')) {
-            router.navigate({ pathname: 'browser' });
-            return false;
-          }
-          return true;
-        }}
+        onShouldStartLoadWithRequest={handleOnShouldStartLoadWithRequest}
       />
     </SafeAreaView>
   );
